e1bc01c2d4c5e782efd3a1ef912dd9b199d2fd8b
Jeff Maher <jeff.m.maher@gmail.com>
2013-07-14 15:17:15 -0400

    Revert "Updated static path"
    This reverts commit 017d1365625ff97caf06b0bb0cc8d0bc58995db1.

--- site/reclaimcities/settings/settings.py
+++ site/reclaimcities/settings/settings.py
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/home/pacdc1/webapps/beta_django/reclaimcities-web/site/reclaimcities/apps/web/static',
+	'/home/pacdc1/webapps/beta_django/reclaimcities-web/reclaimcities/apps/web/static',
 )
 
 # List of finder classes that know how to find static files in
